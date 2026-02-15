466c2b4a8ebc7c3e4d5178c9c5d9db9baef9c338
user <user@MacBook-Air--Abdumannon-Rabbimqulov.local>
2026-02-15 20:08:39 +0500

    DRF

--- test1/urls.py
+++ test1/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from test1.views import get_info,create_product
+from test1.views import get_info,create_product,list_product
 
 urlpatterns=[
     path('get_info/',get_info),
-    path('create_p/',create_product)
+    path('create_p/',create_product),
+    path('list/',list_product),
 
 ]
--- test1/views.py
+++ test1/views.py
@@ -1,8 +1,9 @@
 from django.core.serializers import serialize
 from django.shortcuts import render
+from pydantic.v1 import ValidationError
 
 from rest_framework.response import Response
-
+from .models import Product
 from rest_framework.decorators import api_view
 from .serializers import ProductSerialiser
 
@@ -26,7 +27,17 @@ def create_product(request):
         }
 
         return Response(data)
+    raise ValidationError(serializer.errors)
 
-
+@api_view(['GET'])
+def list_product(request):
+    product=Product.objects.all().order_by('-id')
+    serializer=ProductSerialiser(product,many=True)
+    data = {
+        'success': True,
+        'message': 'productlar',
+        'data':serializer.data
+    }
+    return Response(data)
 
 
