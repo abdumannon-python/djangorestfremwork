c2f1aab656246624e16a317d928542517df65c5d
user <user@MacBook-Air--Abdumannon-Rabbimqulov.local>
2026-02-15 20:32:51 +0500

    DRF

--- test1/urls.py
+++ test1/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from test1.views import get_info,create_product,list_product
+from test1.views import get_info,create_product,list_product,delete_product
 
 urlpatterns=[
     path('get_info/',get_info),
     path('create_p/',create_product),
     path('list/',list_product),
+    path('delete/<int:pk>',delete_product),
 
 ]
--- test1/views.py
+++ test1/views.py
@@ -1,12 +1,9 @@
-from django.core.serializers import serialize
-from django.shortcuts import render
-from pydantic.v1 import ValidationError
-
+from   rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 from .models import Product
 from rest_framework.decorators import api_view
 from .serializers import ProductSerialiser
-
+from rest_framework import status
 @api_view(['GET'])
 def get_info(request):
     data={
@@ -39,5 +36,18 @@ def list_product(request):
         'data':serializer.data
     }
     return Response(data)
-
+@api_view(['DELETE',])
+def delete_product(request,pk):
+    product = Product.objects.filter(pk=pk).first()
+    if product is None :
+        return Response(
+            {'success': False, 'message': 'Malumot topilmadi'},
+            status=status.HTTP_404_NOT_FOUND
+        )
+    product.delete()
+    data = {
+            'success': True,
+            'massege': 'produc ochirildi',
+        }
+    return Response(data)
 
